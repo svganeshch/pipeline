01ee75643b3e453636e2d522df42e50a40051d75
ganeshi4u <ganeshvarmai4u@gmail.com>
2020-01-04 00:52:12 +0530

    Initial pipeline scripts in groovy
    - Not gonna lie its been fun and pain playing around groovy

---
+++ ArrowOS_Builds.groovy
@@ -0,0 +1,199 @@
+import java.io.*;
+import java.sql.*; 
+import groovy.sql.Sql
+
+def MAIN_DISK = "/source"
+def SOURCE_DIR = MAIN_DISK + "/arrow"
+def CCACHE_DIR = MAIN_DISK + "/.ccache"
+def STALE_PATHS_FILE = MAIN_DISK + "/temp_paths.txt"
+
+currentBuild.displayName = "${DEVICE}"
+currentBuild.description = "Waiting for Executor @ ${ASSIGNED_NODE}"
+
+if(!ASSIGNED_NODE.isEmpty()) {
+    node(ASSIGNED_NODE) {
+        currentBuild.description = "Executing @ ${ASSIGNED_NODE}"
+
+        stage('Exporting vars') {
+            sh  '''#!/bin/bash
+
+                    # Linux exports
+                    export PATH=~/bin:$PATH
+                    export USE_CCACHE=1
+                    export CCACHE_COMPRESS=1
+                    export CCACHE_DIR='''+CCACHE_DIR+'''
+                    export KBUILD_BUILD_USER=release
+                    export KBUILD_BUILD_HOST=ArrowOS
+                    export LOCALVERSION=-Arrow
+
+                    # Rom exports
+                    export SELINUX_IGNORE_NEVERALLOWS=true
+                    export ALLOW_MISSING_DEPENDENCIES=true
+                    export ARROW_OFFICIAL=true
+
+                    cd '''+SOURCE_DIR+'''
+                '''
+        }
+
+        stage('Fetching configs from DB') {
+            /*sh  '''#!/bin/bash
+                    ssh root@31.14.133.234 -L 3307:127.0.0.1:3306 -N &
+                    dbcon_pid=$!
+                    sleep 10s
+
+                    set -x
+                '''*/
+            echo "Establishing connection to configs DB...!"
+            device_config_fetch(DEVICE)
+        }
+
+        stage("Hard reset") {
+            try {
+                def stale_file = new File(STALE_PATHS_FILE)
+                if(stale_file.exists()) {
+                    BufferedReader br = new BufferedReader(new FileReader(stale_file))
+
+                    String stale_content;
+                    while((stale_content = br.readLine()) != null) {
+                        echo "---------------------------------"
+                        echo "Deleting stale repos from previous build"
+		                echo "---------------------------------"
+
+                        def dir = new File(stale_content)
+                        if(dir.exists()) {
+                            if(dir.deleteDir()) {
+                                echo "Deleted path ${stale_content}"
+                            }
+                        }
+                    }
+                } else {
+                    echo "---------------------------------"
+	                echo "No temp paths file found!"
+	                echo "---------------------------------"
+                }
+            } catch (e) {
+                throw(e)
+            }
+        }
+    }
+}
+
+/* -----------------------------------------------------
+// Database connection stage
+-------------------------------------------------------*/
+environment {
+    // device config holders
+    def lunch_override_name = null
+    def repo_paths = null
+    def repo_clones = null
+    def repo_clones_paths = null
+    def repopick_topics = null
+    def repopick_changes = null
+
+    // device data (switch holders)
+    def lunch_override_state = null
+    def force_clean = null
+    def test_build = null
+    def is_official = null
+    def buildtype = null
+    def bootimage = null
+    def changelog = null
+    def xda_link = null
+    def global_override = null
+    def default_buildtype_state = null
+}
+
+@NonCPS
+public def setConfigsData(Boolean isDevOvr, String whichDevice, Boolean isGlobalOvr) {
+    try {
+        def dbcon = Sql.newInstance('jdbc:mysql://localhost:3306/configs',
+            'root', 'abansode', 'com.mysql.jdbc.Driver')
+
+        def md = dbcon.connection.metaData
+        def deviceTable = md.getTables(null, null, whichDevice, null)
+
+        try {
+            while(deviceTable.next()) {
+                if(deviceTable.getString('TABLE_NAME') == whichDevice) {
+                    dbcon.eachRow("select * from "+whichDevice+"") {
+                        configs->
+                        if(whichDevice == "common_config" && isGlobalOvr) {
+                            global_override = configs.global_override
+                            if(global_override == "yes") {
+                                echo "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"
+                                echo "Fetching global overrides!"
+                                echo "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"
+
+                                echo "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"
+                                echo "Global overrides set to ${global_override}"
+                                echo "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"
+
+                                env.force_clean = configs.force_clean
+                                env.test_build = configs.test_build
+                                env.is_official = configs.is_official
+                                env.bootimage = configs.bootimage
+                                env.buildtype = configs.buildtype
+                                env.default_buildtype_state = configs.default_buildtype_state
+
+                                if(env.default_buildtype_state == "yes") {
+                                    dbcon.eachRow("SELECT * FROM "+DEVICE+"") {
+                                        env.buildtype = configs.default_buildtype
+                                    }
+                                }
+                                return
+                            }
+                        }
+
+                        env.repo_paths = isDevOvr ? configs.ovr_repo_paths : configs.repo_paths
+                        env.repo_clones = isDevOvr ? configs.ovr_repo_clones : configs.repo_clones
+                        env.repo_clones_paths = isDevOvr ? configs.ovr_repo_clones_paths : configs.repo_clones_paths
+                        env.repopick_topics = isDevOvr ? configs.ovr_repopick_topics : configs.repopick_topics
+                        env.repopick_changes = isDevOvr ? configs.ovr_repopick_changes : configs.repopick_changes
+                        env.force_clean = isDevOvr ? configs.ovr_force_clean : configs.force_clean
+                        env.test_build = isDevOvr ? configs.ovr_test_build : configs.test_build
+                        env.is_official = isDevOvr ? configs.ovr_is_official : configs.is_official
+                        env.buildtype = isDevOvr ? configs.ovr_buildtype : configs.buildtype
+                        env.bootimage = isDevOvr ? configs.ovr_bootimage : configs.bootimage
+                        env.changelog = isDevOvr ? configs.ovr_changelog : configs.changelog
+
+                        if(whichDevice != "common_config") {
+                            env.lunch_override_name = configs.lunch_override_name
+                            env.lunch_override_state = configs.lunch_override_state
+                            env.xda_link = isDevOvr ? configs.ovr_xda_link : configs.xda_link
+                        }
+                    }
+                } else {
+                    echo "Failed to find table ${whichDevice}"
+                }
+            }
+        } finally {
+            dbcon.close()
+        }
+    } catch(e) {
+        throw(e)
+    }
+}
+
+public def device_config_fetch(def DEVICE) {
+    try {
+        echo "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"
+        echo "Fetching configuration for ${DEVICE}"
+        echo "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"
+
+        setConfigsData(true, DEVICE, false)
+        if(env.lunch_override_name != null && !env.lunch_override_name.isEmpty() && env.lunch_override_state != "no") {
+            echo "Lunch override set to ${env.lunch_override_name}"
+            boolean lunch_override = true
+            //set global overrides
+            setConfigsData(false, "common_config", true)
+            return
+        }
+
+        //set normal device
+        setConfigsData(false, DEVICE, false)
+        //set global overrides
+        setConfigsData(false, "common_config", true)
+    } catch(e) {
+        throw(e)
+    }
+}
---
+++ arrow_build_kicker.groovy
@@ -0,0 +1,62 @@
+import groovy.json.JsonSlurper
+
+String calcDate() { ['date', '+%Y%m%d'].execute().text.trim()}
+String calcTimestamp() { ['date', '+%s'].execute().text.trim()}
+
+int NO_OF_NODES = 3
+def jsonParse(def json) { new groovy.json.JsonSlurperClassic().parseText(json) }
+nodeStructureUrl = "https://raw.githubusercontent.com/ArrowOS/android_vendor_arrow/arrow-10.0/node_structure.json".toURL()
+officialDevicesUrl = "https://raw.githubusercontent.com/ArrowOS/android_vendor_arrow/arrow-10.0/arrow.devices".toURL()
+def activeDevices = active_devices.split(",");
+
+@NonCPS
+String getDeviceHal(device) {
+    String gotHal = null
+    officialDevicesUrl.eachLine {
+        if(it.charAt(0) != null && it.charAt(0) != "#") {
+            if(it.split(" ")[0] == device) {
+                gotHal = it.split(" ")[2]
+            }
+        }
+    }
+    return gotHal
+}
+
+node("master") {
+    stage("Assigning Nodes!") {
+        try {
+            String nodeStructure = nodeStructureUrl.text
+            def nodeStJson = jsonParse(nodeStructure)
+    
+            if(activeDevices != null && activeDevices.length !=0 && activeDevices[0] != "none") {
+                for(device in activeDevices) {
+                    String assign_node = null
+                    String devHal = getDeviceHal(device)
+                    for(i=1; i<=NO_OF_NODES; i++) {
+                        if(nodeStJson["arrow-"+i][0]["hals"].contains(devHal)) {
+                            assign_node = "Arrow-${i}"
+                        }
+                    }
+    
+                    if(assign_node != null && !assign_node.isEmpty() && assign_node != "none") {
+                        echo "Triggering build for ${device} on ${assign_node}"
+                        build job: 'ArrowOS_Builds', parameters: [
+                            string(name: 'DEVICE', value: device),
+                            string(name: 'ASSIGNED_NODE', value: assign_node),
+                            string(name: 'BUILD_TIMESTAMP', value: calcDate() + calcTimestamp())
+                        ], propagate: false, wait: false
+                        sleep 2
+                    } else {
+                        echo "No node assigned for ${device}"
+                        continue
+                    }
+                }
+            } else {
+                echo "No active devices passed!"
+            }
+        } catch (e) {
+            currentBuild.result = "FAILED"
+            throw e
+        }
+    }
+}
