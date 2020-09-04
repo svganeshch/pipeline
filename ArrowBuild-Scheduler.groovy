95053b67ed9c1837d87ea70915330a6978053e3e
Ganesh Varma <ganeshvarmai4u@gmail.com>
2020-09-04 16:56:10 +0530

    Scheduler: Hardcode version for community builds

--- ArrowBuild-Scheduler.groovy
+++ ArrowBuild-Scheduler.groovy
@@ -130,7 +130,7 @@ node("master") {
                                 string(name: 'DEVICE', value: n4dev),
                                 string(name: 'ASSIGNED_NODE', value: "Arrow-4"),
                                 string(name: 'BUILD_TIMESTAMP', value: calcDate() + calcTimestamp()),
-                                string(name: 'VERSION', value: version)
+                                string(name: 'VERSION', value: "arrow-10.0")
                             ], propagate: false, wait: false
                             sleep 2
                         }
