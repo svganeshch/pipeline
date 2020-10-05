31bb7ba1fb284db1573c577b69299517176313af
Ganesh Varma <ganeshvarmai4u@gmail.com>
2020-10-05 19:11:29 +0530

    Builder: Use groovy truth to perform null/empty check

--- Arrow-Builder.groovy
+++ Arrow-Builder.groovy
@@ -48,7 +48,7 @@ public void sendSlackNotify(def msg, def consoleUrl = null, def downUrl = null) 
                         ]
                     ]
                     
-    if (slackThreadResp == null || slackThreadResp.empty) {
+    if (!slackThreadResp) {
         slackThreadResp = slackSend(channel: "#arrowos-jenkins", blocks: msgBlock)
     } else {
         slackSend(
