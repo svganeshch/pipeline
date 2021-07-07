ee34c421d34d3a94d874cc8b25b2ad0e169d6781
Ganesh Varma <ganeshvarmai4u@gmail.com>
2021-07-07 13:41:08 +0530

    Builder: We initially upload everything to the same place
    - Update upload URLs all over

--- Arrow-Builder.groovy
+++ Arrow-Builder.groovy
@@ -836,17 +836,9 @@ public def upload() {
             if [ -f $TO_UPLOAD ]; then
                 if [ '''+env.test_build+''' = "yes" ]; then
                     if [ '''+IS_COMMUNITY+''' = "true" ]; then
-                        if [ '''+ASSIGNED_NODE+''' = "Arrow-5" ]; then
-                            script -q -c "yes | rsync -rav --info=progress2 $TO_UPLOAD root@get.mirror1.arrowos.net:/mnt/HDD1/builds/arrow-$(echo '''+env.TG_ARROW_VERSION+''' | cut -d "v" -f 2)/community_experiments/'''+DEVICE+'''/" | stdbuf -oL tr '\r' '\n'
-                        else
-                            script -q -c "yes | rsync -rav --info=progress2 $TO_UPLOAD root@10.0.0.200:/mnt/HDD1/builds/arrow-$(echo '''+env.TG_ARROW_VERSION+''' | cut -d "v" -f 2)/community_experiments/'''+DEVICE+'''/" | stdbuf -oL tr '\r' '\n'
-                        fi
+                           script -q -c "yes | rsync -rav --info=progress2 $TO_UPLOAD root@get.mirror1.arrowos.net:/mnt/HDD1/builds/arrow-$(echo '''+env.TG_ARROW_VERSION+''' | cut -d "v" -f 2)/community_experiments/'''+DEVICE+'''/" | stdbuf -oL tr '\r' '\n'
                     else
-                        if [ '''+ASSIGNED_NODE+''' = "Arrow-5" ]; then
-                            script -q -c "yes | rsync -rav --info=progress2 $TO_UPLOAD root@get.mirror1.arrowos.net:/mnt/HDD1/builds/'''+VERSION+'''/experiments/'''+DEVICE+'''/" | stdbuf -oL tr '\r' '\n'
-                        else
-                            script -q -c "yes | rsync -rav --info=progress2 $TO_UPLOAD root@10.0.0.200:/mnt/HDD1/builds/'''+VERSION+'''/experiments/'''+DEVICE+'''/" | stdbuf -oL tr '\r' '\n'
-                        fi
+                           script -q -c "yes | rsync -rav --info=progress2 $TO_UPLOAD root@get.mirror1.arrowos.net:/mnt/HDD1/builds/'''+VERSION+'''/experiments/'''+DEVICE+'''/" | stdbuf -oL tr '\r' '\n'
                     fi
                     if [ $? -eq 0 ]; then
                         echo "SUCCESSFULLY UPLOADED TEST BUILD TO ARROW SERVER"
@@ -859,17 +851,9 @@ public def upload() {
                     echo TG_TITLE "**New ['''+DEVICE+''']($TG_DOWN_URL) build [(`date +'%d-%m-%Y'`)](https://changelog.arrowos.net) is out! ('''+VERSION+''')**" >> '''+env.TG_VARS_FILE+'''
                 else
                     if [ '''+IS_COMMUNITY+''' = "true" ]; then
-                        if [ '''+ASSIGNED_NODE+''' = "Arrow-5" ]; then
-                            script -q -c "yes | rsync -rav --info=progress2 $TO_UPLOAD root@get.mirror1.arrowos.net:/mnt/HDD1/builds/arrow-$(echo '''+env.TG_ARROW_VERSION+''' | cut -d "v" -f 2)/community/'''+DEVICE+'''/" | stdbuf -oL tr '\r' '\n'
-                        else
-                            script -q -c "yes | rsync -rav --info=progress2 $TO_UPLOAD root@10.0.0.200:/mnt/HDD1/builds/arrow-$(echo '''+env.TG_ARROW_VERSION+''' | cut -d "v" -f 2)/community/'''+DEVICE+'''/" | stdbuf -oL tr '\r' '\n'
-                        fi
+                           script -q -c "yes | rsync -rav --info=progress2 $TO_UPLOAD root@get.mirror1.arrowos.net:/mnt/HDD1/builds/arrow-$(echo '''+env.TG_ARROW_VERSION+''' | cut -d "v" -f 2)/community/'''+DEVICE+'''/" | stdbuf -oL tr '\r' '\n'
                     else
-                        if [ '''+ASSIGNED_NODE+''' = "Arrow-5" ]; then
-                            script -q -c "yes | rsync -rav --info=progress2 $TO_UPLOAD root@get.mirror1.arrowos.net:/mnt/HDD1/builds/'''+VERSION+'''/official/'''+DEVICE+'''/" | stdbuf -oL tr '\r' '\n'
-                        else
-                            script -q -c "yes | rsync -rav --info=progress2 $TO_UPLOAD root@10.0.0.200:/mnt/HDD1/builds/'''+VERSION+'''/official/'''+DEVICE+'''/" | stdbuf -oL tr '\r' '\n'
-                        fi
+                           script -q -c "yes | rsync -rav --info=progress2 $TO_UPLOAD root@get.mirror1.arrowos.net:/mnt/HDD1/builds/'''+VERSION+'''/official/'''+DEVICE+'''/" | stdbuf -oL tr '\r' '\n'
                     fi
                     if [ $? -eq 0 ]; then
                         echo "SUCCESSFULLY UPLOADED TO ARROW SERVERS"
