f2e028329e7c99190843bac9481aad83e19d31a2
Lineu Martins <lineumartins001@gmail.com>
2022-12-25 22:09:36 +0100

    update db connection credentials

--- Arrow-Builder.groovy
+++ Arrow-Builder.groovy
@@ -424,7 +424,7 @@ public def setConfigsData(String whichDevice, Boolean isGlobalOvr) {
         def which_db = IS_COMMUNITY == "true" ? VERSION + "_community" : VERSION
         which_db = DEVICE_PROFILE == "official" ? which_db : "test_profile_" + which_db
         def dbcon = Sql.newInstance('jdbc:mysql://host.docker.internal:3306/configs_' +which_db,
-            'jenkins', 'jenkinsarrowinthearse', 'com.mysql.jdbc.Driver')
+            'arrow_jenkins', 'sjfwy6MX4PBc3Qs5', 'com.mysql.jdbc.Driver')
 
         def md = dbcon.connection.metaData
         def deviceTable = md.getTables(null, null, whichDevice, null)
